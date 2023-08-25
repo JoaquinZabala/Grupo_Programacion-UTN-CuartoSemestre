a998d51ebc772192ac6ab4ea59699aa58c27a852
CamilaGonzalez97 <112906352+CamilaGonzalez97@users.noreply.github.com>
2023-08-24 23:09:50 -0300

    Commit clase 1

---
+++ e-commerce2022/client/js/client/js/products.js
@@ -0,0 +1,38 @@
+
+const productos = [
+
+{
+    id:1,
+    productName: "Lapiz",
+    price: 200,
+    quanty: 1,
+    img: "/client/media/lapiz.png",
+
+
+},
+{
+    id:2,
+    productName: "Lapicera",
+    price: 300,
+    quanty: 1,
+    img: "/client/media/lapicera.png",
+
+},
+{
+    id:3,
+    productName: "Mochila",
+    price: 16000,
+    quanty: 1,
+    img: "/client/media/mochila.png",
+
+},
+{
+    id:4,
+    productName: "Cuaderno",
+    price: 4200,
+    quanty: 1,
+    img: "/client/media/cuaderno.png",
+
+},
+
+] ;
