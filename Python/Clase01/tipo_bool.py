bff8d0a2346dc00aaeb28a53f9281ed3a06334e8
CamilaGonzalez97 <112906352+CamilaGonzalez97@users.noreply.github.com>
2023-09-08 18:50:05 -0300

    Add files via upload

---
+++ Python/Clase01/tipo_bool.py
@@ -0,0 +1,19 @@
+
+#Bool contiene los valores de True y False
+#Los tipos numéricos, es false para el 0 (cero), true para los demás valores
+valor = 0
+resultado = (bool(valor)
+print(f'valor: {valor}, Resultado); {resultado}')
+
+valor = 0.1
+resultado = bool(valor)
+print(f'valor:{valor}, Resultado: {resultado}')
+
+#Tipo string -> False '', True demás valores
+valor = ''
+resultado = (bool(valor)
+print(f'valor: {valor}, Resultado); {resultado}')
+
+valor = 'Hola'
+resultado = (bool(valor)
+print(f'valor: {valor}, Resultado); {resultado}')
