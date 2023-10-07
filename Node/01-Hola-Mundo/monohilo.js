8da978fdbffa99bb64bfd777f813065f16c2971f
JoaquinZabala <111473041+JoaquinZabala@users.noreply.github.com>
2023-10-06 21:08:52 -0300

    Update monohilo.js

--- Node/01-Hola-Mundo/monohilo.js
+++ Node/01-Hola-Mundo/monohilo.js
@@ -1,12 +1,14 @@
-console.log("Hola a toda la cohorte 2022")
 
-var i = 0;
+console.log('Hola a toda la cohorte 2022')
+
+var i =0;
 setInterval(function(){
     console.log(i);
     i++;
-    // if(i === 5){
-    //     console.log("forzamos un error");
-    //     var a = 3+z;
-    // }
+
+    //if( i === 5){
+    //  console.log('Forzamos un error');
+    //  var a = 3 + z;
+    //} 
 }, 1000);
-console.log("Segunda instrucción");
+console.log('Ultima instruccion')
