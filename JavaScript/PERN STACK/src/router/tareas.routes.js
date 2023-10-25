953ebbe4b46a9b6236c93e569c3b09b316d74887
NahuelAlaniz <104447527+NahuelAlaniz@users.noreply.github.com>
2023-10-24 21:24:26 -0300

    Update tareas.routes.js

--- JavaScript/PERN STACK/src/router/tareas.routes.js
+++ JavaScript/PERN STACK/src/router/tareas.routes.js
@@ -1,16 +1,17 @@
 import  Router  from "express-promise-router";
 import {actualizarTarea, crearTarea, eliminarTarea, listarTarea, listarTarea} from "../controllers/tarea.controller.js"
+import {isAuth} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
-router.get('/tareas',listarTarea);
+router.get('/tareas',isAuth,listarTarea);
 
-router.get('/tareas/:id',listarTarea);
+router.get('/tareas/:id',isAuth,listarTarea);
 
-router.post('/tareas',crearTarea)
+router.post('/tareas',isAuth,crearTarea)
 
-router.put('/tareas/:id',actualizarTarea);
+router.put('/tareas/:id',isAuth,actualizarTarea);
 
-router.delete('/tarea/:id',eliminarTarea );
+router.delete('/tarea/:id',isAuth,eliminarTarea );
 
 export default router;
