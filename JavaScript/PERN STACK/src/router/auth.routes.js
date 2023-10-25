30979fc67bc439df9a28baa959501d95e38ac21e
NahuelAlaniz <104447527+NahuelAlaniz@users.noreply.github.com>
2023-10-24 21:26:34 -0300

    Update auth.routes.js

--- JavaScript/PERN STACK/src/router/auth.routes.js
+++ JavaScript/PERN STACK/src/router/auth.routes.js
@@ -1,5 +1,6 @@
 import Router from "express promise router"; 
 import {signin, signup, signout, profile} from "../controllers/auth.controller.js"
+import { isAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router ();
 
@@ -9,6 +10,6 @@ router.post("/signup", singnup);
 
 router.get("/profile", signout);
 
-router.get("/profile", profile);
+router.get("/profile",isAuth, profile);
 
 export default router;
