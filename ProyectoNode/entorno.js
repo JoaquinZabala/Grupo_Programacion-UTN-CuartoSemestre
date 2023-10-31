3f84bb94137ddc58f4fa15738e7b026206cd8c92
JoaquinZabala <joacozabala21@gmail.com>
2023-10-30 21:29:56 -0300

    Revert "CuartoSemestre"
    This reverts commit 40f553c066eba9de4d17cd9fee420667079a15e6.

--- ProyectoNode/entorno.js
+++
@@ -1,8 +0,0 @@
-
-let nombre = process.env.NOMBRE || 'Sin Nombre';
-let web = process.env.MI_WEB || 'No tengo web';
-
-console.log('Hola '+nombre);
-console.log('Mi web es: '+web);
-
-console.log('Ultima parte');
